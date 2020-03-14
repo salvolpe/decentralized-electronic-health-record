b5997d76e3ae2adbd6c4cd788739ce518a449a41
akilhylton <hyltonakil@gmail.com>
2020-03-13 21:21:01 -0400

    displays a simple map

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from 'mapbox-gl';
+import "./App.css"
 
-function App() {
+const DEMO_API_KEY = 'pk.eyJ1IjoiYWh5bHRvbjE5IiwiYSI6ImNrN3FrYnFxOTAwOGUzc21jYWFtNzh4aG4ifQ.mXUbDQrYWm7TnEexZJ4f1Q';
+
+mapboxgl.accessToken = DEMO_API_KEY;
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    };
+  }
+ componentDidMount() {
+   // eslint-disable-next-line
+    const map = new mapboxgl.Map({ 
+    container: this.mapContainer,
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [this.state.lng, this.state.lat],
+    zoom: this.state.zoom
+  });
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mapContainer" ref={el => this.mapContainer = el} />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
